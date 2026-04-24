b417f3bd08fe5b3b06fdbc4b5dfffb3d3bb856d8
Min Phone Thit <billy@Mins-MacBook-Air.local>
2026-04-24 18:53:29 +0700

    nextjs docs chapter15

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+const LoadDashBoard = () => {
+  return <DashboardSkeleton />;
+};
+
+export default LoadDashBoard;
